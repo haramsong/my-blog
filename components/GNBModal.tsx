7b5410cd9ad0535559a4c27c0c7a095a91ce4ae4
haramsong <alakazam1324@naver.com>
2025-04-25 14:20:00 +0900

    🐛 GNB Modal에도 스크롤 추가

--- components/GNBModal.tsx
+++ components/GNBModal.tsx
@@ -30,7 +30,7 @@ export default function GNBModal({ onClose }: GNBModalProps) {
       />
 
       <aside
-        className={`absolute top-0 left-0 h-full w-[250px] bg-white dark:bg-gray-900 shadow-lg transform transition-transform duration-300 ease-in-out ${
+        className={`absolute top-0 left-0 h-full overflow-auto w-[250px] bg-white dark:bg-gray-900 shadow-lg transform transition-transform duration-300 ease-in-out ${
           visible ? "translate-x-0" : "-translate-x-full"
         }`}
         onClick={(e) => e.stopPropagation()}

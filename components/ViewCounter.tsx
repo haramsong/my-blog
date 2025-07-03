65aa28403649753c889885a61d85dbf72f7bdeae
haramsong <alakazam1324@gmail.com>
2025-07-03 14:00:07 +0900

    🎨 API 엔드포인트 분리

--- components/ViewCounter.tsx
+++ components/ViewCounter.tsx
@@ -1,7 +1,8 @@
-// components/ViewCounter.tsx
 "use client";
 
 import { useEffect, useState } from "react";
+
+import { increaseViewCount } from "@/lib/api/view";
 
 export default function ViewCounter({ slug }: { slug: string }) {
   const [views, setViews] = useState<number>(0);
@@ -15,31 +16,16 @@ export default function ViewCounter({ slug }: { slug: string }) {
   }
 
   useEffect(() => {
-    const endpoint = `${process.env.NEXT_PUBLIC_API_ENDPOINT}/view-count`;
-    if (!endpoint) {
-      console.error("API endpoint is not defined");
-      return;
+    async function fetchViews() {
+      try {
+        const currentViews = await increaseViewCount(slug);
+        setViews(currentViews);
+      } catch (err) {
+        console.error(err);
+      }
     }
 
-    const increaseViews = async () => {
-      try {
-        const res = await fetch(endpoint, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            "x-api-token": process.env.NEXT_PUBLIC_API_SECRET ?? "",
-          },
-          body: JSON.stringify({ slug }),
-        });
-
-        const data = await res.json();
-        setViews(data.views ?? 0);
-      } catch (err) {
-        console.error("조회수 증가 실패:", err);
-      }
-    };
-
-    increaseViews();
+    fetchViews();
   }, [slug]);
 
   return <p>{`${formatViews(views)}`}</p>;

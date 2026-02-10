db8476a209054a2a4ce2ea3702da3fcd0e330b89
haramsong <alakazam1324@gmail.com>
2026-02-10 14:09:49 +0900

    fix: 일부 텍스트 수정

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,7 +4,7 @@ export default function NotFound() {
   return (
     <section className="flex flex-col items-center w-full h-full justify-center text-center px-4">
       <h1 className="text-4xl font-bold mb-4 text-gray-800 dark:text-gray-100">
-        페이지를 찾을 수 없어요
+        페이지를 찾을 수 없어여
       </h1>
       <p className="text-gray-600 dark:text-gray-400 mb-6">
         요청하신 페이지가 존재하지 않거나 이동되었어요.

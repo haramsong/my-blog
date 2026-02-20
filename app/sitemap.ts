81d35635050bcde1020478c40492240ea51c61be
haramsong <alakazam1324@gmail.com>
2026-02-20 13:14:49 +0900

    🔍 sitemap 축소

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,26 +1,27 @@
 import { MetadataRoute } from "next";
-import { getTagsWithCount, getGNBTree, getPostMeta } from "@/lib/posts";
+import { getPostMeta } from "@/lib/posts";
+// import { getTagsWithCount, getGNBTree, getPostMeta } from "@/lib/posts";
 
 export const dynamic = "force-static";
 export default function sitemap(): MetadataRoute.Sitemap {
-  const categoryMeta = getGNBTree();
+  // const categoryMeta = getGNBTree();
   const postMeta = getPostMeta();
-  const tags = getTagsWithCount();
+  // const tags = getTagsWithCount();
 
-  const sitemapDefault: MetadataRoute.Sitemap = [
-    {
-      url: `${process.env.NEXT_PUBLIC_BASE_URL}/`,
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.3,
-    },
-    {
-      url: `${process.env.NEXT_PUBLIC_BASE_URL}/profile/`,
-      lastModified: new Date(),
-      changeFrequency: "yearly",
-      priority: 0.2,
-    },
-  ];
+  // const sitemapDefault: MetadataRoute.Sitemap = [
+  //   {
+  //     url: `${process.env.NEXT_PUBLIC_BASE_URL}/`,
+  //     lastModified: new Date(),
+  //     changeFrequency: "monthly",
+  //     priority: 0.3,
+  //   },
+  //   {
+  //     url: `${process.env.NEXT_PUBLIC_BASE_URL}/profile/`,
+  //     lastModified: new Date(),
+  //     changeFrequency: "yearly",
+  //     priority: 0.2,
+  //   },
+  // ];
 
   const sitemapFromPosts: MetadataRoute.Sitemap = postMeta.map((post) => {
     return {
@@ -31,34 +32,34 @@ export default function sitemap(): MetadataRoute.Sitemap {
     };
   });
 
-  const sitemapFromCategories: MetadataRoute.Sitemap = Object.keys(
-    categoryMeta
-  ).flatMap((section) => {
-    return Object.keys(categoryMeta[section]).map((category) => {
-      return {
-        url: `${process.env.NEXT_PUBLIC_BASE_URL}/posts/${section}/${category}/`,
-        lastModified: new Date(),
-        changeFrequency: "monthly",
-        priority: 0.4,
-      };
-    });
-  });
+  // const sitemapFromCategories: MetadataRoute.Sitemap = Object.keys(
+  //   categoryMeta
+  // ).flatMap((section) => {
+  //   return Object.keys(categoryMeta[section]).map((category) => {
+  //     return {
+  //       url: `${process.env.NEXT_PUBLIC_BASE_URL}/posts/${section}/${category}/`,
+  //       lastModified: new Date(),
+  //       changeFrequency: "monthly",
+  //       priority: 0.4,
+  //     };
+  //   });
+  // });
 
-  const sitemapFromTags: MetadataRoute.Sitemap = Object.keys(tags).map(
-    (tag) => {
-      return {
-        url: `${process.env.NEXT_PUBLIC_BASE_URL}/tags/${tag}/`,
-        lastModified: new Date(),
-        changeFrequency: "monthly",
-        priority: 0.1,
-      };
-    }
-  );
+  // const sitemapFromTags: MetadataRoute.Sitemap = Object.keys(tags).map(
+  //   (tag) => {
+  //     return {
+  //       url: `${process.env.NEXT_PUBLIC_BASE_URL}/tags/${tag}/`,
+  //       lastModified: new Date(),
+  //       changeFrequency: "monthly",
+  //       priority: 0.1,
+  //     };
+  //   }
+  // );
 
   return [
-    ...sitemapDefault,
+    // ...sitemapDefault,
     ...sitemapFromPosts,
-    ...sitemapFromCategories,
-    ...sitemapFromTags,
+    // ...sitemapFromCategories,
+    // ...sitemapFromTags,
   ];
 }
